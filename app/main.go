35e66c0d6f80ff634f41f2bbccd19426d9464540
reyhanmichiels <michiels.reyhan@gmail.com>
2023-07-22 16:42:12 +0700

    load env configuration

---
+++ app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "bcc-university/src/sdk/conf"
+
+func init() {
+	conf.LoadEnv()
+}
+
+func main() {
+	
+}
---
+++ src/sdk/conf/conf.go
@@ -0,0 +1,16 @@
+package conf
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading env file")
+	}
+
+}
