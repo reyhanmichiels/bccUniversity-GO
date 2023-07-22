ad0f5d09834ca4026123bde0687613f97ac89f73
reyhanmichiels <michiels.reyhan@gmail.com>
2023-07-22 16:46:12 +0700

    make connection to sql database with gorm

--- app/main.go
+++ app/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "bcc-university/src/sdk/conf"
+import (
+	"bcc-university/src/sdk/conf"
+	"bcc-university/src/sdk/db/sql"
+)
 
 func init() {
 	conf.LoadEnv()
+	sql.ConnectToDb()
 }
 
 func main() {
-	
+
 }
