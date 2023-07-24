29d94b8ffd71bf8f088c057cb2c940242e087135
reyhanmichiels <michiels.reyhan@gmail.com>
2023-07-24 21:03:56 +0700

    create student model

---
+++ src/business/entity/student.go
@@ -0,0 +1,9 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Student struct {
+	gorm.Model
+	User_id           uint   `json:"user_id" gorm:"type:uint"`
+	Student_id_number string `json:"student_id_number" gorm:"type:varchar(20)"`
+}
