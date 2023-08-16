e07c9d1a7ea2b268a3944ff929cbf7134163be9c
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-16 23:35:00 +0700

    refactor(course table): updating datatype and constraint

--- src/business/entity/course.go
+++ src/business/entity/course.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Course struct {
 	gorm.Model
-	Name    string   `json:"name" gorm:"type:varchar(50)"`
-	Credit  int      `json:"credit" gorm:"type:int"`
+	Name    string   `json:"name" gorm:"type:varchar(50);unique;notnull"`
+	Credit  int      `json:"credit" gorm:"type:int;notnull"`
 	Classes []*Class `json:"classes"`
 }
