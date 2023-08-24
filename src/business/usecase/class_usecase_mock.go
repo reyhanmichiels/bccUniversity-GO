60e10f375d148406261514378628014146c73149
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-24 19:46:29 +0700

    test(GetAllClass): make mock for GetAllClassUseCase

--- src/business/usecase/class_usecase_mock.go
+++ src/business/usecase/class_usecase_mock.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"bcc-university/src/business/entity"
+	"bcc-university/src/sdk/library"
 
 	"github.com/stretchr/testify/mock"
 )
@@ -10,43 +11,50 @@ type ClassUseCaseMock struct {
 	Mock mock.Mock
 }
 
-func GetAllClassUseCase() ([]entity.ClassApi, interface{}) {
+func (classUseCaseMock *ClassUseCaseMock) GetAllClassUseCase() ([]entity.ClassApi, interface{}) {
 
+	args := classUseCaseMock.Mock.Called()
+
+	if args[1] != nil {
+
+		return nil, args[1].(library.ErrorObject)
+
+	}
 	return []entity.ClassApi{}, nil
 
 }
 
-func RemoveUserFromClassUseCase(loginUser entity.User, classId uint, userId uint) interface{} {
+func (classUseCaseMock *ClassUseCaseMock) RemoveUserFromClassUseCase(loginUser entity.User, classId uint, userId uint) interface{} {
 
 	return nil
 
 }
 
-func AdmAddUserToClassUseCase(loginUser entity.User, classId uint, userId uint) interface{} {
+func (classUseCaseMock *ClassUseCaseMock) AdmAddUserToClassUseCase(loginUser entity.User, classId uint, userId uint) interface{} {
 
 	return nil
 
 }
 
-func CreateClassUseCase(userInput entity.CreateUpdateClassBind, loginUser entity.User) (entity.CreateUpdateClassApi, interface{}) {
+func (classUseCaseMock *ClassUseCaseMock) CreateClassUseCase(userInput entity.CreateUpdateClassBind, loginUser entity.User) (entity.CreateUpdateClassApi, interface{}) {
 
 	return entity.CreateUpdateClassApi{}, nil
 
 }
 
-func EditClassUseCase(userInput entity.CreateUpdateClassBind, loginUser entity.User, classId uint) (entity.CreateUpdateClassApi, interface{}) {
+func (classUseCaseMock *ClassUseCaseMock) EditClassUseCase(userInput entity.CreateUpdateClassBind, loginUser entity.User, classId uint) (entity.CreateUpdateClassApi, interface{}) {
 
 	return entity.CreateUpdateClassApi{}, nil
 
 }
 
-func DeleteClassUseCase(loginUser entity.User, classId uint) interface{} {
+func (classUseCaseMock *ClassUseCaseMock) DeleteClassUseCase(loginUser entity.User, classId uint) interface{} {
 
 	return nil
 
 }
 
-func GetClassParticipantUseCase(loginUser entity.User, classId uint) (entity.ClassParticipantApi, interface{}) {
+func (classUseCaseMock *ClassUseCaseMock) GetClassParticipantUseCase(loginUser entity.User, classId uint) (entity.ClassParticipantApi, interface{}) {
 
 	return entity.ClassParticipantApi{}, nil
 
