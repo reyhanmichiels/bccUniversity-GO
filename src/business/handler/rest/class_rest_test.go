329bf72ec8ecdac8fbe7557f639d2399b2c5561c
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-24 20:49:02 +0700

    test(GetAllClass): make unit test path  2

--- src/business/handler/rest/class_rest_test.go
+++ src/business/handler/rest/class_rest_test.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"bcc-university/src/business/entity"
 	"bcc-university/src/business/usecase"
 	"bcc-university/src/sdk/library"
 	"encoding/json"
@@ -61,6 +62,71 @@ func TestGetAllClassPath1(t *testing.T) {
 			assert.Equal(t, http.StatusInternalServerError, response.Code, "status code should be equal")
 			assert.Equal(t, "test", jsonResponse["message"], "message should be equal")
 			assert.Equal(t, "error", jsonResponse["status"], "status should be equal")
+
+			functionCall.Unset()
+
+		})
+
+	}
+
+}
+
+func TestGetAllClassPath2(t *testing.T) {
+
+	for i := 1; i <= 5; i++ {
+
+		t.Run(fmt.Sprintf("path 2 get all class testing %d", i), func(t *testing.T) {
+
+			classResponse := []entity.ClassApi{
+				{
+					Name:        "testname1",
+					Course_id:   1,
+					Participant: 1,
+					ClassCode:   "testcode1",
+				},
+				{
+					Name:        "testname1",
+					Course_id:   1,
+					Participant: 1,
+					ClassCode:   "testcode1",
+				},
+			}
+			functionCall := classUsecaseMock.Mock.On("GetAllClassUseCase").Return(classResponse, nil)
+
+			engine := gin.Default()
+			engine.GET("/api/v1/classes", classRest.GetAllClass)
+
+			response := httptest.NewRecorder()
+			request, err := http.NewRequest("GET", "/api/v1/classes", nil)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+			engine.ServeHTTP(response, request)
+
+			var jsonResponse map[string]any
+			err = json.Unmarshal(response.Body.Bytes(), &jsonResponse)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+
+			assert.Equal(t, http.StatusOK, response.Code, "status code should be equal")
+			assert.Equal(t, "successfully get all class", jsonResponse["message"], "message should be equal")
+			assert.Equal(t, "success", jsonResponse["status"], "status should be equal")
+
+			for i, v := range jsonResponse["data"].([]interface{}) {
+
+				v := v.(map[string]any)
+
+				assert.Equal(t, classResponse[i].Name, v["name"], fmt.Sprintf("name %d should be equal", i+1))
+				assert.Equal(t, classResponse[i].Course_id, uint(v["course_id"].(float64)), fmt.Sprintf("course id %d should be equal", i+1))
+				assert.Equal(t, classResponse[i].Participant, int(v["participant"].(float64)), fmt.Sprintf("participant %d should be equal", i+1))
+				assert.Equal(t, classResponse[i].ClassCode, v["class_code"], fmt.Sprintf("class code %d should be equal", i+1))
+
+			}
 
 			functionCall.Unset()
 
