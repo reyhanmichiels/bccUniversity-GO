6b34ca99bbc848063ec40f607f63a999003309be
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-24 18:51:08 +0700

    test(claim student number): make unit test path 3

--- src/business/handler/rest/student_rest_test.go
+++ src/business/handler/rest/student_rest_test.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"bcc-university/src/business/entity"
 	"bcc-university/src/business/usecase"
 	"bcc-university/src/sdk/library"
 	"encoding/json"
@@ -97,6 +98,52 @@ func TestClaimStudentNumberPath2(t *testing.T) {
 			assert.Equal(t, http.StatusInternalServerError, response.Code, "status code should be equal")
 			assert.Equal(t, "test", jsonResponse["message"], "message should be equal")
 			assert.Equal(t, "error", jsonResponse["status"], "status should be equal")
+
+			functionCall.Unset()
+
+		})
+
+	}
+
+}
+
+func TestClaimStudentNumberPath3(t *testing.T) {
+
+	for i := 1; i <= 5; i++ {
+
+		claimStudentNumberApi := entity.ClaimStudentNumberApi{
+			Student_id_number: "test",
+		}
+		functionCall := studentUseCaseMock.Mock.On("ClaimStudentNumberUseCase", getLoginUser()).Return(claimStudentNumberApi, nil)
+
+		t.Run(fmt.Sprintf("path 3 claim student number testing %d", i), func(t *testing.T) {
+
+			engine := gin.Default()
+			engine.GET("/api/v1/user/student-number", setUserLogin, studentRest.ClaimStudentNumber)
+
+			response := httptest.NewRecorder()
+			request, err := http.NewRequest("GET", "/api/v1/user/student-number", nil)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+			engine.ServeHTTP(response, request)
+
+			var jsonResponse map[string]any
+			err = json.Unmarshal(response.Body.Bytes(), &jsonResponse)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+
+			data := jsonResponse["data"].(map[string]any)
+
+			assert.Equal(t, http.StatusOK, response.Code, "status code should be equal")
+			assert.Equal(t, "Succesed claim student number", jsonResponse["message"], "message should be equal")
+			assert.Equal(t, "success", jsonResponse["status"], "status should be equal")
+			assert.Equal(t, "test", data["student_id_number"], "student id number should be equal")
 
 			functionCall.Unset()
 
