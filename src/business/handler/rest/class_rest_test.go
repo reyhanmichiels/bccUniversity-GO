682352bb495f790924ecdd056a7c2637abb88efc
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-25 14:32:00 +0700

    test(AdmAddUserToClass):make unit test path 1

--- src/business/handler/rest/class_rest_test.go
+++ src/business/handler/rest/class_rest_test.go
@@ -332,3 +332,39 @@ func TestRemoveUserFromClassPath5(t *testing.T) {
 	}
 
 }
+
+func TestAdmAddUserToClassPath1(t *testing.T) {
+
+	for i := 1; i <= 5; i++ {
+
+		t.Run(fmt.Sprintf("path 1 remove user from class testing %d", i), func(t *testing.T) {
+
+			engine := gin.Default()
+			engine.POST("/api/v1/class/:classId/user/:userId", classRest.RemoveUserFromClass)
+
+			response := httptest.NewRecorder()
+			request, err := http.NewRequest("POST", "/api/v1/class/test/user/test", nil)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+			engine.ServeHTTP(response, request)
+
+			var jsonResponse map[string]any
+			err = json.Unmarshal(response.Body.Bytes(), &jsonResponse)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+
+			assert.Equal(t, http.StatusBadRequest, response.Code, "http status code should be equal")
+			assert.Equal(t, "failed to convert user id to int", jsonResponse["message"], "message should be equal")
+			assert.Equal(t, "error", jsonResponse["status"], "status should be equal")
+
+		})
+
+	}
+
+}
