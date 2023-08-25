6b0ce0be227a863518080f090c3d9a636de2ef2b
reyhanmichiels <michiels.reyhan@gmail.com>
2023-08-25 18:13:14 +0700

    test(EditClass): make unit test path 5

--- src/business/handler/rest/class_rest_test.go
+++ src/business/handler/rest/class_rest_test.go
@@ -789,7 +789,6 @@ func TestCreateClassPath4(t *testing.T) {
 			}
 
 			dataResponse := jsonResponse["data"].(map[string]any)
-			fmt.Println(dataResponse)
 			courseResponse := dataResponse["Course"].(map[string]any)
 
 			assert.Equal(t, http.StatusCreated, response.Code, "http status code should be equal")
@@ -1021,7 +1020,7 @@ func TestEditClassPath4(t *testing.T) {
 
 	for i, v := range userInput {
 
-		t.Run(fmt.Sprintf("path 3 Edit Class testing %d", i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("path 4 Edit Class testing %d", i), func(t *testing.T) {
 
 			errObject := library.ErrorObject{
 				Code:    http.StatusInternalServerError,
@@ -1061,6 +1060,92 @@ func TestEditClassPath4(t *testing.T) {
 			assert.Equal(t, "test", jsonResponse["message"], "message should be equal")
 			assert.Equal(t, "error", jsonResponse["status"], "status should be equal")
 			assert.Equal(t, "test", jsonResponse["error"], "error should be equal")
+
+			functionCall.Unset()
+
+		})
+
+	}
+
+}
+
+func TestEditClassPath5(t *testing.T) {
+
+	userInput := []entity.CreateUpdateClassBind{
+		{
+			Name:      "testname1",
+			Course_id: 1,
+		},
+		{
+			Name:      "testname2",
+			Course_id: 2,
+		},
+		{
+			Name:      "testname3",
+			Course_id: 3,
+		},
+		{
+			Name:      "testname4",
+			Course_id: 4,
+		},
+		{
+			Name:      "testname5",
+			Course_id: 5,
+		},
+	}
+
+	for i, v := range userInput {
+
+		t.Run(fmt.Sprintf("path 5 Edit Class testing %d", i), func(t *testing.T) {
+
+			classApi := entity.CreateUpdateClassApi{
+				Name:        v.Name,
+				Course_id:   v.Course_id,
+				Participant: i,
+				ClassCode:   "test",
+			}
+			classApi.Course.Name = "test"
+			classApi.Course.Credit = i
+			functionCall := classUsecaseMock.Mock.On("EditClassUseCase", v, getLoginUser(), uint(i)).Return(classApi, nil)
+
+			engine := gin.Default()
+			engine.POST("/api/v1/class/:classId", setUserLogin, classRest.EditClass)
+
+			jsonInput, err := json.Marshal(v)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+
+			response := httptest.NewRecorder()
+			request, err := http.NewRequest("POST", fmt.Sprintf("/api/v1/class/%d", i), bytes.NewBuffer(jsonInput))
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+			engine.ServeHTTP(response, request)
+
+			var jsonResponse map[string]any
+			err = json.Unmarshal(response.Body.Bytes(), &jsonResponse)
+			if err != nil {
+
+				t.Fatal(err.Error())
+
+			}
+			dataResponse := jsonResponse["data"].(map[string]any)
+			courseResponse := dataResponse["Course"].(map[string]any)
+
+			assert.Equal(t, http.StatusOK, response.Code, "http status code should be equal")
+			assert.Equal(t, "success", jsonResponse["status"], "status should be equal")
+			assert.Equal(t, "successfully edit class", jsonResponse["message"], "message should be equal")
+			assert.Equal(t, classApi.Name, dataResponse["name"], "name should be equal")
+			assert.Equal(t, classApi.ClassCode, dataResponse["class_code"], "class_code should be equal")
+			assert.Equal(t, classApi.Course_id, uint(dataResponse["course_id"].(float64)), "course_id should be equal")
+			assert.Equal(t, classApi.Participant, int(dataResponse["participant"].(float64)), "participant should be equal")
+			assert.Equal(t, classApi.Course.Name, courseResponse["name"], "course name should be equal")
+			assert.Equal(t, classApi.Course.Credit, int(courseResponse["credit"].(float64)), "course credit should be equal")
 
 			functionCall.Unset()
 
